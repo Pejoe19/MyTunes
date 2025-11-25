6f46cef7ffdfe9f28958487b73af13beb5fa2502
PC-Pernille <pernilleajorgensen@gmail.com>
2025-11-25 14:59:22 +0100

    Added so you can see all playlists (with songs and time)

--- src/main/java/dk/easv/mytunes/Be/Playlist.java
+++ src/main/java/dk/easv/mytunes/Be/Playlist.java
@@ -1,13 +1,17 @@
 package dk.easv.mytunes.Be;
 
+
 public class Playlist {
 
     private int id;
     private String name;
+    private int numberOfSongs = 0;
+    private double playTime = 0.00;
 
     public Playlist(int id, String name) {
         this.id = id;
         this.name = name;
+
     }
 
     public int getId() {
@@ -24,6 +28,22 @@ public class Playlist {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public void setNumberOfSongs(int numberOfSongs) {
+        this.numberOfSongs = numberOfSongs;
+    }
+
+    public double getPlayTime() {
+        return playTime;
+    }
+
+    public void setPlayTime(double playTime) {
+        this.playTime = playTime;
     }
 
     @Override
--- src/main/java/dk/easv/mytunes/GUI/MainController.java
+++ src/main/java/dk/easv/mytunes/GUI/MainController.java
@@ -30,6 +30,8 @@ public class MainController {
     private void loadPlaylists(){
         // Tells the table which properties of the playlist to show in which columns
         tblCoPLName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCoPLSongs.setCellValueFactory(new PropertyValueFactory<>("numberOfSongs"));
+        tblCoPLTime.setCellValueFactory(new PropertyValueFactory<>("playTime"));
         try {
             // Gets the data from model
             TvPlaylists.setItems(model.loadPlaylists());
